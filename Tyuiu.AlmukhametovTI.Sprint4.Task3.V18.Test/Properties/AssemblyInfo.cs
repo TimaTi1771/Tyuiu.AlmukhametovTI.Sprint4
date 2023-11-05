3ccf1cd515aee2fb03cfaa4f0568518add306b10
TimaTi1771 <talmuhametov@yandex.ru>
2023-11-05 21:20:07 +0300

    Task3

---
+++ Tyuiu.AlmukhametovTI.Sprint4.Task3.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AlmukhametovTI.Sprint4.Task3.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AlmukhametovTI.Sprint4.Task3.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93f7b60d-e29a-4c83-96e4-ab226af3d48c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.AlmukhametovTI.Sprint4.Task3.V18/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AlmukhametovTI.Sprint4.Task3.V18.Lib;
+
+namespace Tyuiu.AlmukhametovTI.Sprint4.Task3.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 / Выполнил: Альмухаметов Т. И. | ИИПб-23-3";
+
+            Console.WriteLine("**********************************************************************");
+            Console.WriteLine("* Спринт #4                                                          *");
+            Console.WriteLine("* Задание #3                                                         *");
+            Console.WriteLine("* Вариант #18                                                        *");
+            Console.WriteLine("**********************************************************************");
+            Console.WriteLine("* УСЛОВИЕ: Дан двумерный целочисленный массив 5 на 5 элементов,      *");
+            Console.WriteLine("* заполненный статическими значениями в диапазоне от 2 до 9.         *");
+            Console.WriteLine("* Найдите максимальный элемент в последней строке массива.           *");
+            Console.WriteLine("* {2, 4, 2, 7, 7}  {4, 7, 9, 9, 7}  {8, 5, 5, 6, 5}  {7, 7, 5, 2, 9} *");
+            Console.WriteLine("* {9, 3, 7, 9, 8}                                                    *");
+            Console.WriteLine("**********************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                   *");
+            Console.WriteLine("**********************************************************************");           
+
+            int[,] array = new int[5, 5] {{ 2, 4, 2, 7, 7 },
+                                          { 4, 7, 9, 9, 7 },
+                                          { 8, 5, 5, 6, 5 },
+                                          { 7, 7, 5, 2, 9 },
+                                          { 9, 3, 7, 9, 8 }};
+
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            Console.WriteLine("Массив: ");
+            
+            for (int i = 0; i < rows; i++)
+            {
+                for ( int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();           
+            Console.WriteLine("**********************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                          *");
+            Console.WriteLine("**********************************************************************");
+            int res = ds.Calculate(array);
+
+            Console.WriteLine("Максимальный элемент в последней строке массива: " + res);
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
